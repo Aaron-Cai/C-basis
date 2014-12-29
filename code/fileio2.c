3042e099f720b905f7768f2253e431fe14fd7466
Aaron <cjy_mail@foxmail.com>
2014-12-29 17:14:49 +0800

    remove the comment of file2.c

--- code/fileio2.c
+++ code/fileio2.c
@@ -1,26 +1,26 @@
-// #include<stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// //#include <iostream>
-// #define _CRT_SECURE_NO_WARNINGS
-// 
-// int main(int argc, char *argv[])
-// {
-// 	FILE *file4read		= fopen("test1.txt","r");
-// 	FILE *file4write	= fopen("out.txt","w");
-// 	char str[100];
-// 	char str2[100];
-// 	int x;
-// 
-// 	if(file4read != 0)
-// 	{
-// 		while(fscanf(file4read,"%s %d%s",str,&x,str2)!=EOF)
-// 			printf("%s %d%s\n",str,x,str2);
-// 		fclose(file4read);
-// 	}
-// 	else
-// 	{
-// 		printf("file does not exist\n");
-// 	}
-// 	return 0;
-// }
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main(int argc, char *argv[])
+{
+	FILE *file4read		= fopen("test1.txt","r");
+	FILE *file4write	= fopen("out.txt","w");
+	char str[100];
+	char str2[100];
+	int x;
+
+	if(file4read != 0)
+	{
+		while(fscanf(file4read,"%s %d%s",str,&x,str2)!=EOF)
+			printf("%s %d%s\n",str,x,str2);
+		fclose(file4read);
+	}
+	else
+	{
+		printf("file does not exist\n");
+	}
+	return 0;
+}
