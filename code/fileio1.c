a295b5524aade40b13b0a1e30b6ca5b6743a0c4b
Aaron <cjy_mail@foxmail.com>
2014-12-29 17:12:27 +0800

    add code and slide

---
+++ code/fileio1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main(int argc, char* argv[])
+{
+	if(argc != 3)
+	{
+		printf("usage: %s plaintext outfile",argv[0]);
+	}
+	else
+	{
+		FILE *plaintext		= fopen(argv[1],"r");
+		FILE *ciphertext	= fopen(argv[2],"w");
+		char str[100];
+		int x;
+		char key = 't';
+		if(plaintext == 0)
+		{
+			printf("The input file does not exist, please check the file name of plaintext file\n");
+			return -1;
+		}
+		printf("encrypting...\n");
+		while((x = fgetc(plaintext)) != EOF)
+		{
+			if(x - 'a' >=0 && x - 'a'<26)
+			{
+				int x1 = (x - 'a' +  key - 'a' ) % 26 + 'a';
+				fputc(x1,ciphertext);
+			}
+			else
+			{
+				fputc(x,ciphertext);
+			}
+		}
+		printf("encrypting done.\n");
+		fclose(plaintext);
+		fclose(ciphertext);
+	}
+	return 0;
+}
